50e7289faf5ac07c371706fb414bac8d2f6bb612
Elmer Orellana <104410892+ssblue18@users.noreply.github.com>
2022-09-01 12:20:48 -0700

    rename database

--- src/main/java/util/HibernateConf.java
+++ src/main/java/util/HibernateConf.java
@@ -18,7 +18,7 @@ public class HibernateConf {
 
             Properties settings = new Properties();
             settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-            settings.put(Environment.URL, "jdbc:mysql://localhost:3306/Project1-ERS");
+            settings.put(Environment.URL, "jdbc:mysql://localhost:3306/project1_ers");
             settings.put(Environment.USER, "root");
             settings.put(Environment.PASS, "Pita0514");
 
