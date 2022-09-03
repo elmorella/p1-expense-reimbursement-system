5ba9642b21f9d1534279fb0e0094c8ffd94087ce
Elmer Orellana <104410892+ssblue18@users.noreply.github.com>
2022-09-02 20:34:17 -0700

    removed commented code

--- src/main/java/servlets/LoginServlet.java
+++ src/main/java/servlets/LoginServlet.java
@@ -3,13 +3,10 @@ package servlets;
 import doa.dao.EmployeeDao;
 import doa.factories.EmployeeDaoFactory;
 import model.Employee;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
